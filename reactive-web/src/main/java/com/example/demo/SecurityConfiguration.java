01c1a83f790601aaaff4a8224bc7fe44079f9a83
Matt Raible <matt.raible@okta.com>
2018-11-05 13:42:57 -0700

    Merge from master and disable security

--- reactive-web/src/main/java/com/example/demo/SecurityConfiguration.java
+++ reactive-web/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,5 +1,4 @@
-/*
-package com.example.demo;
+/*package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,43 +7,19 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-import java.util.Collections;
-
-@Configuration
-@EnableWebFluxSecurity
-@EnableReactiveMethodSecurity
+@EnableWebFluxSecurity 
 public class SecurityConfiguration {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
-            .csrf()
-                .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
             .authorizeExchange()
-                .pathMatchers("/ws/**").permitAll()
                 .anyExchange().authenticated()
                 .and()
             .oauth2Login()
                 .and()
             .oauth2ResourceServer()
                 .jwt().and().and().build();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-        configuration.setAllowedMethods(Collections.singletonList("GET"));
-        configuration.setAllowedHeaders(Collections.singletonList("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-}
-*/
+        }
+}*/
